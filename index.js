aa314ebfc52e3107268db6dacce98b4fc7c6924a
Konan <kixeyems0@gmail.com>
2024-07-01 11:39:03 +0100

    vf

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import geoip from "geoip-lite";
 
 const app = express();
 
@@ -8,15 +7,14 @@ app.get("/hello", (req, res) => {
   const clientIp =
     req.headers["x-forwarded-for"] || req.socket.remoteAddress || "";
 
-  const ipv4 = clientIp.split(":").pop();
+  // Use Vercel's geolocation headers
+  const country = req.headers["x-vercel-ip-country"] || "Unknown";
+  const city = req.headers["x-vercel-ip-city"] || "Unknown";
 
-  console.log(ipv4);
-  const geo = geoip.lookup(ipv4); // Get geolocation info
-
-  const location = geo ? `${geo.city}, ${geo.country}` : "Unknown";
+  const location = `${city}, ${country}`;
 
   res.json({
-    Client_ip: ipv4,
+    Client_ip: clientIp,
     location: location,
     greeting: `Hello ${visitor_name}, the temperature is 30 degrees Celsius in ${location}`,
   });
