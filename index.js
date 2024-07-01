d686ee99415181ba0279d4d3666a987fa662e8b4
Konan <kixeyems0@gmail.com>
2024-07-01 11:34:31 +0100

    wde

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const express = require("express");
-// const geoip = require("geoip-lite");
+import express from "express";
+import geoip from "geoip-lite";
 
 const app = express();
 
 app.get("/hello", (req, res) => {
   const visitor_name = req.query.visitor_name;
-  const clientIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  const clientIp =
+    req.headers["x-forwarded-for"] || req.socket.remoteAddress || "";
 
   const ipv4 = clientIp.split(":").pop();
 
   console.log(ipv4);
-  // const geo = geoip.lookup(ipv4.toString()); // Get geolocation info
+  const geo = geoip.lookup(ipv4); // Get geolocation info
 
-  // const location = geo ? `${geo.city}, ${geo.country}` : "Unknown";
+  const location = geo ? `${geo.city}, ${geo.country}` : "Unknown";
 
   res.json({
     Client_ip: ipv4,
-    // location: location,
-    // greeting: `Hello ${visitor_name}, the temperature is 30 degrees Celsius in ${location}`,
+    location: location,
+    greeting: `Hello ${visitor_name}, the temperature is 30 degrees Celsius in ${location}`,
   });
 });
 
